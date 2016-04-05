775110efa9991700782be13418ceec5cdcd6b844
Blake Bless and Ryan Carlile <blake@>
2016-04-05 14:09:17 -0700

    initial commit with inputs that take movies, age, and times to account for ticket price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
 // business logic
+function ticket(movie,time,age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+}
+ticket.prototype.price = function() {
+  if (this.age <= 18) {
+    this.price = "$" + 5
+  } else if (this.age >= 65){
+    this.price = "$" + 6
+  } else {
+    this.price = "$" + 10
+  }
+  return this.price;
+}
+
 
 
 
 // // end business logic
 //
 // user user logic
+$(document).ready(function() {
+
+
+$("form.choices").submit(function(event) {
+   event.preventDefault();
+
+    var inputMovie =$("#movie").val();
+    var inputTime = $('#time').val();
+    var inputAge = parseInt($("input#age").val());
+    var newTicket = new ticket(inputMovie, inputTime, inputAge);
+    $("#output").append("<p>" + newTicket.price() + "</p>");
+    
+console.log(inputMovie);
+  });
+});
 
 // end user logic
